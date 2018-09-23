bc3d8d2a709bbcfdddeca93bcbf2060232e4cb25
Andrii Molodetskyi <amolodetskyi@gmail.com>
2018-09-23 20:50:03 +0300

    correction(main.js, style.css): after review

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,68 @@
 (function() {
+    let elTabMenu,
+        elPagesWrapper,
+        elCloseImagePopupButton,
+        elLargeImage,
+        elImagePopupWrapper;
 
-let elTabMenu,
-    elPagesWrapper,
-    elCloseImagePopupButton,
-    elLargeImage,
-    elImagePopupWrapper;
+    const elGalleryCollection = document.getElementsByClassName("image-wrapper"),
+          elTabButtonCollection = document.getElementsByClassName("tab-button"),
+          elTabPageCollection = document.getElementsByClassName("tab-page");
 
-const elGalleryCollection = document.getElementsByClassName("image-wrapper"),
-      elTabButtonCollection = document.getElementsByClassName("tab-button"),
-      elTabPageCollection = document.getElementsByClassName("tab-page");
-
-try {
-    elTabMenu = getElementById("tab-menu");
-    elPagesWrapper = getElementById("pages-wrapper");
-    elCloseImagePopupButton = getElementById("close-image-popup");
-    elLargeImage = getElementById("large-image");
-    elImagePopupWrapper = getElementById("image-popup-wrapper");
-
-} catch (e) {
-    console.error('Failed to init elements: ' + e.message);
-}
-
-function getElementById(id) {
-    const element = document.getElementById(id);
-    if (element === null) {
-        throw new Error(`No element "${id}"`);
+    function getElementById(id) {
+        const element = document.getElementById(id);
+        if (!element) {
+            throw new Error(`No element "${id}"`);
+        }
+        return element;
     }
-    return element;
-}
 
-Array.prototype.forEach.call(elTabButtonCollection, (item,i)=>{
-    item.addEventListener("click",  function(){
-        const elActiveButton = elTabMenu.getElementsByClassName("active")[0];
-        const elActivePage = elPagesWrapper.getElementsByClassName("active")[0];
-        elActiveButton.classList.remove("active");
-        elActivePage.classList.remove("active");
+    try {
+        elTabMenu = getElementById("tab-menu");
+        elPagesWrapper = getElementById("pages-wrapper");
+        elCloseImagePopupButton = getElementById("close-image-popup");
+        elLargeImage = getElementById("large-image");
+        elImagePopupWrapper = getElementById("image-popup-wrapper");
 
-        event.currentTarget.classList.add("active");
-        elTabPageCollection[i].classList.add("active");
+    } catch (e) {
+        console.error("Failed to init elements: " + e.message);
+    }
+
+    Array.prototype.forEach.call(elTabButtonCollection, (item, i)=>{
+        item.addEventListener("click",  function(){
+            const elActiveButton = elTabMenu.getElementsByClassName("active")[0];
+            const elActivePage = elPagesWrapper.getElementsByClassName("active")[0];
+            elActiveButton.classList.remove("active");
+            elActivePage.classList.remove("active");
+
+            event.currentTarget.classList.add("active");
+            elTabPageCollection[i].classList.add("active");
+        });
     });
-});
 
-function openImagePopup(element){
-    if(!element.classList.contains("open"))
-        element.classList.add("open");
-}
+    function openImagePopup(element){
+        if(!element.classList.contains("open"))
+            element.classList.add("open");
+    }
 
-function closeImagePopup(element) {
-    if(element.classList.contains("open"))
-        element.classList.remove("open");
-}
+    function closeImagePopup(element) {
+        if(element.classList.contains("open"))
+            element.classList.remove("open");
+    }
 
-function addLargeImage (collection, elPopup, elImage){
-    for(let i = 0; i<collection.length; i++){
-        let elChosenImage = collection[i];
-        elChosenImage.addEventListener("click",()=>{
-            elImage.src = elChosenImage.getElementsByTagName("img")[0].src;
-            openImagePopup(elPopup);
+    function addLargeImage(collection, elPopup, elImage){
+        Array.prototype.forEach.call(collection, (item, i)=>{
+            item.addEventListener("click", ()=>{
+                elImage.src = item.getElementsByTagName("img")[0].src;
+                openImagePopup(elPopup);
+            })
         });
     }
-}
+    addLargeImage(elGalleryCollection, elImagePopupWrapper,elLargeImage);
 
-addLargeImage(elGalleryCollection, elImagePopupWrapper,elLargeImage);
-
-elCloseImagePopupButton.addEventListener("click", ()=>{
-    closeImagePopup(elImagePopupWrapper);
-});
+    elCloseImagePopupButton.addEventListener("click", ()=>{
+        closeImagePopup(elImagePopupWrapper);
+    });
 
 })();
 
